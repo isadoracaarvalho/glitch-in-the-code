65ff7a0374c11006731b733816049a51597a74a4
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-06-13 16:08:10 -0300

    getAllTitles.js

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -1,7 +1,12 @@
 const movies = require("../data/movies");
 
 const getAllTitles = (movies) => {
- 
+
+  const resultado = movies.map(function(movie) {
+    return movie.titulo;
+  });
+
+  return resultado;
 }
 
 module.exports = {
