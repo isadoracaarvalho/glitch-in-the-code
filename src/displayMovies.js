620f8e2029d21fe2161e9c288aff85d193f06c15
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-08-13 18:05:07 -0300

    implementa displayMovies.js

--- src/displayMovies.js
+++ src/displayMovies.js
@@ -1,7 +1,13 @@
 const { movies } = require("../data/list1");
 
 const displayMovies = (movies) => {
- 
+
+  if (movies.length === 0) return
+
+  movies.forEach(movie => {
+    console.log(`Título: ${movie.titulo} - Diretor: ${movie.diretor}`);
+  });
+
 }
 
 module.exports = {
