cedc7340f3b1b839728d2a2fba96447f3cbad5ad
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-08-13 17:07:55 -0300

    implementa highestRatedMovie.jd

--- src/highestRatedMovie.js
+++ src/highestRatedMovie.js
@@ -1,7 +1,15 @@
 const movies = require("../data/movies");
 
 const highestRatedMovie = (movies) => {
-  
+
+  if( movies.length === 0 ) return undefined
+
+  const resultado = movies.reduce((anterior, atual) => {
+    return atual.avaliacao > anterior.avaliacao ? atual : anterior
+  });
+
+  return resultado
+
 }
 
 module.exports = {
