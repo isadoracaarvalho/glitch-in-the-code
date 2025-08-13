9a19ee9f78c0964efb529680a87156b1fd97714e
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-08-13 18:37:44 -0300

    implementa orderMoviesByYear.js

--- src/orderMoviesByYear.js
+++ src/orderMoviesByYear.js
@@ -2,6 +2,13 @@ const movies = require("../data/movies");
 
 const orderMoviesByYear = (movies) => {
   
+  if( movies.length === 0 ) return []
+
+  const resultado = movies.toSorted((a, b) => {
+    return (a.anoLancamento > b.anoLancamento) ? 1 : -1
+  })
+
+  return resultado
 }
 
 module.exports = {
